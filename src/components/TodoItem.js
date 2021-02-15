4a6b0af7bad741ad0cca7186d4a8c46137e98ec6
Nurkadyr <nurkadyrurmatbekov@gmail.com>
2021-02-15 18:03:27 +0600

    done done

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+
 class TodoItem extends Component {
     state = {
         important: false,
@@ -6,6 +7,7 @@ class TodoItem extends Component {
     }
 
     updateState = (state) => {
+        
         return {
             important: !state.important
         }
@@ -19,13 +21,21 @@ class TodoItem extends Component {
         this.setState(this.updateState, this.postUpdate)
     }
 
+
+
+    onClickDone = () => {
+        this.setState((state)=>{
+            return {done:!state.done}
+        })
+    }
+
     render() {
         const className = this.state.important ? "list-group-item todo-item active" : "todo-item list-group-item"
-        const spanClassName = 'text done'
+        const spanClassName = this.state.done?'text done':"text"
         return (
             <li className={className}>
                 <span onClick={this.onClick} className={spanClassName} >{this.props.children}</span>
-                <button className="btn btn-success">Done</button>
+                <button onClick={this.onClickDone} className="btn btn-success">Done</button>
             </li>
         );
     }
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -4,9 +4,10 @@ import TodoItem from './TodoItem';
 const TodoList = () => {
     return (
         <ul className="list-group mt-2">
-            <TodoItem >Read Book</TodoItem>
-            <TodoItem >Cook</TodoItem>
-            <TodoItem >Sleep</TodoItem>
+            <TodoItem>Read Book</TodoItem>
+            <TodoItem>Cook</TodoItem>
+            <TodoItem>Sleep</TodoItem>
+            <TodoItem></TodoItem>
         </ul>
     );
 };
